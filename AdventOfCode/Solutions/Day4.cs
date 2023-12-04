adf3c8c9c79d824b232616baca8de8470d209399
Sloan Kelly <sloankelly@gmail.com>
2023-12-04 17:58:20 -0500

    Day 4, solution 1 is done

---
+++ AdventOfCode/Solutions/Day4.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions
+{
+    public class Day4 : BaseSolution
+    {
+        protected override void OnRun()
+        {
+            int total = 0;
+            Args[0].ReadAllLines()
+                   .Select(line => Deserialize(line))
+                   .ForEach(card =>
+                   {
+                       WriteLine($"Card #{card.ID} score {card.Score}");
+                       total += card.Score;
+                   });
+
+            WriteLine($"Total: {total}");
+        }
+
+        Card Deserialize(string line)
+        {
+            string[] lineSplit = line.StripAndTrim(':');
+            int gameId = int.Parse(lineSplit[0].Substring("Game ".Length).Trim());
+            Card card = new Card(gameId);
+            string[] sides = lineSplit[1].StripAndTrim('|');
+            card.Winning.AddRange(Parse(sides[0]));
+            card.Numbers.AddRange(Parse(sides[1]));
+            return card;
+        }
+
+        IEnumerable<int> Parse(string line)
+        {
+            return line.StripAndTrim(' ')
+                       .Select(x => int.Parse(x));
+        }
+
+    }
+
+    internal class Card
+    {
+        public int ID { get; private set; }
+
+        public int Score
+        {
+            get
+            {
+                int score = 0;
+                foreach (var num in Numbers)
+                {
+                    if (Winning.Contains(num))
+                    {
+                        score++;
+                    }
+                }
+
+                if (score == 0) return 0;
+
+                return (int)Math.Pow(2, score - 1);
+            }
+        }
+
+        public List<int> Winning { get; private set; } = new List<int>();
+
+        public List<int> Numbers { get; private set; } = new List<int>();
+
+        public Card(int id)
+        {
+            ID = id;
+        }
+    }
+}
---
+++ AdventOfCode/Solutions/IEnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solutions
+{
+    internal static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (var item in source)
+            {
+                action(item);
+            }
+
+            return source;
+        }
+    }
+}
