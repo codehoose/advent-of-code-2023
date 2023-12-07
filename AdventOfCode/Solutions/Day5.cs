71d5bb3a593b750d917749460b906d5ef1f5bef8
Sloan Kelly <sloankelly@gmail.com>
2023-12-06 20:10:40 -0500

    Day 5, solution 1

--- AdventOfCode/Solutions/Day5.cs
+++ AdventOfCode/Solutions/Day5.cs
@@ -1,16 +1,122 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Solutions
 {
     internal class Day5 : BaseSolution
     {
+        string[] maps = new string[]
+        {
+            "seed-to-soil",
+            "soil-to-fertilizer",
+            "fertilizer-to-water",
+            "water-to-light",
+            "light-to-temperature",
+            "temperature-to-humidity",
+            "humidity-to-location"
+        };
+
         protected override void OnRun()
         {
-            throw new NotImplementedException();
+            string[] lines = "data\\day5\\input.txt".ReadAllLines();
+            Planting planting = new Planting(lines);
+
+            long[] values = planting.Seeds.ToArray();
+
+            List<Tuple<long, long>> mappings = new List<Tuple<long, long>>();
+            foreach (var key in maps)
+            {
+                foreach (var seed in values)
+                {
+                    var ranges = planting.Ranges[key].Where(r => r.IsInRange(seed)).ToArray();
+                    if (ranges.Length != 1)
+                    {
+                        mappings.Add(new Tuple<long, long>(seed, seed));
+                    }
+                    else
+                    {
+                        mappings.Add(new Tuple<long, long>(seed, ranges[0].GetDestination(seed)));
+                    }
+                }
+
+                values = mappings.Select(x => x.Item2).ToArray();
+
+                if (key != "humidity-to-location")
+                    mappings.Clear();
+            }
+
+            long[] orderedList = mappings.Select(x => x.Item2).OrderBy(x => x).ToArray();
+            WriteLine($"Smallest range {orderedList[0]}");
+        }
+    }
+
+    internal class Planting
+    {
+        public List<long> Seeds { get; private set; } = new List<long>();
+
+        public Dictionary<string, List<PlantRange>> Ranges { get; private set; } = new Dictionary<string, List<PlantRange>>();
+
+        public Planting(string[] lines)
+        {
+            int i = 0;
+            while (i < lines.Length)
+            {
+                string line = lines[i];
+                if (line.StartsWith("seeds:"))
+                {
+                    Seeds.AddRange(line.Substring("seeds: ".Length)
+                                       .StripAndTrim(' ')
+                                       .Select(x => long.Parse(x)));
+                    i++;
+                }
+                else if (line.EndsWith(" map:"))
+                {
+                    string title = line.Replace(" map:", "").Trim();
+                    Ranges.Add(title, new List<PlantRange>());
+                    i++;
+                    do
+                    {
+                        long[] rangeLine = lines[i].StripAndTrim(' ')
+                                                   .Select(x => long.Parse(x))
+                                                   .ToArray();
+
+                        PlantRange range = new PlantRange(rangeLine[1], rangeLine[0], rangeLine[2]);
+                        Ranges[title].Add(range);
+                        i++;
+                    } while (i < lines.Length && !lines[i].EndsWith(" map:"));
+                }
+            }
+        }
+    }
+
+    internal class PlantRange
+    {
+        public long SourceStart { get; private set; }
+
+        public long DestinationStart { get; private set; }
+
+        public long Length { get; private set; }
+
+        public bool IsInRange(long number)
+        {
+            long offset = number - SourceStart;
+            if (offset < 0 || offset > Length) return false;
+            return true;
+        }
+
+        public long GetDestination(long number)
+        {
+            if (!IsInRange(number)) return number;
+            long offset = number - SourceStart;
+            return DestinationStart + offset;
+        }
+
+        public PlantRange(long source, long destination, long length)
+        {
+            SourceStart = source;
+            DestinationStart = destination;
+            Length = length;
         }
     }
 }
